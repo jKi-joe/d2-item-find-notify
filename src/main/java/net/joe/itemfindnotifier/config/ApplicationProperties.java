a6dc41d4488e23c21a320561c4a24fa4b5e0a761
johannes <johannes@smoope.com>
2018-07-05 11:39:45 +0200

    chore: Adjusted property paths

--- src/main/java/net/joe/itemfindnotifier/config/ApplicationProperties.java
+++ src/main/java/net/joe/itemfindnotifier/config/ApplicationProperties.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 
-@ConfigurationProperties("net.joe")
+@ConfigurationProperties("itemfindnotifier")
 @Getter
 @Setter
 public class ApplicationProperties {
