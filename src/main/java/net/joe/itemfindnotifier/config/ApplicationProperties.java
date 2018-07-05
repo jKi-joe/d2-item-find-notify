c7191a5c000386f22cead04f26e3974eead36de7
johannes <johannes@smoope.com>
2018-07-05 11:46:01 +0200

    chore: Made path variable reading more tolerant

--- src/main/java/net/joe/itemfindnotifier/config/ApplicationProperties.java
+++ src/main/java/net/joe/itemfindnotifier/config/ApplicationProperties.java
@@ -8,18 +8,29 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.List;
 
 @ConfigurationProperties("itemfindnotifier")
-@Getter
-@Setter
 public class ApplicationProperties {
 
+    @Setter
     private String itemFileDirectory;
 
+    public String getItemFileDirectory() {
+        return itemFileDirectory.endsWith("/") ? itemFileDirectory : itemFileDirectory + "/";
+    }
+
+    @Getter
+    @Setter
     private String itemFileName;
 
+    @Getter
+    @Setter
     private String discordWebhookUrl;
 
+    @Getter
+    @Setter
     private List<String> messagePrefixes;
 
+    @Getter
+    @Setter
     private List<String> filters;
 
 }
